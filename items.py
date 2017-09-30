3198c955966d9b03febdd7d850ebfe4ed50dc401
Rico Ullmann <rico@erinnerungsfragmente.de>
2017-09-30 23:05:01 +0200

    streamline needs and triggers

--- items.py
+++ items.py
@@ -6,10 +6,7 @@ pkg_dnf = {
 
 svc_systemd = {
     'postgresql': {
-        'needs': [
-            'pkg_dnf:postgresql-server',
-            'action:postgresql_initdb',
-        ],
+        'needs': ['pkg_dnf:postgresql-server', 'action:postgresql_initdb'],
     },
 }
 
@@ -19,12 +16,8 @@ files = {
         'owner': 'postgres',
         'group': 'postgres',
         'mode': '0600',
-        'needs': [
-            'pkg_dnf:postgresql-server',
-        ],
-        'triggers': [
-            'svc_systemd:postgresql:restart',
-        ],
+        'needs': ['pkg_dnf:postgresql-server'],
+        'triggers': ['svc_systemd:postgresql:restart'],
     },
 }
 
@@ -32,9 +25,7 @@ actions = {
     'postgresql_initdb': {
         'command': 'postgresql-setup --initdb --unit postgresql',
         'unless': 'ls /var/lib/pgsql/initdb_postgresql.log',
-        'needs': [
-            'pkg_dnf:postgresql-server',
-        ],
+        'needs': ['pkg_dnf:postgresql-server'],
     },
 }
 
@@ -46,28 +37,20 @@ for name, config in node.metadata.get('postgresql', {}).get('roles', {}).items()
     postgres_roles[name] = {
         'superuser': config.get('superuser', False),
         'password': config['password'],
-        'needs': [
-            'pkg_dnf:postgresql-server',
-            'action:postgresql_initdb',
-        ],
+        'needs': ['pkg_dnf:postgresql-server', 'action:postgresql_initdb'],
     }
 
 for name, config in node.metadata.get('postgresql', {}).get('databases', {}).items():
     postgres_dbs[name] = {
         'owner': config.get('owner', 'postgres'),
-        'needs': [
-            'pkg_dnf:postgresql-server',
-            'action:postgresql_initdb',
-        ],
+        'needs': ['pkg_dnf:postgresql-server', 'action:postgresql_initdb'],
     }
 
 if node.has_bundle('monit'):
     files['/etc/monit.d/postgresql'] = {
         'source': 'monit',
         'mode': '0640',
-        'triggers': [
-            'svc_systemd:monit:restart',
-        ],
+        'triggers': ['svc_systemd:monit:restart'],
     }
 
 if node.has_bundle('collectd') and node.metadata.get('postgresql', {}).get('collectd', False):
@@ -78,10 +61,6 @@ if node.has_bundle('collectd') and node.metadata.get('postgresql', {}).get('coll
         'source': 'collectd.conf',
         'mode': '0640',
         'content_type': 'mako',
-        'needs': [
-            'pkg_dnf:collectd-postgresql',
-        ],
-        'triggers': [
-            'svc_systemd:collectd:restart',
-        ],
+        'needs': ['pkg_dnf:collectd-postgresql'],
+        'triggers': ['svc_systemd:collectd:restart'],
     }
